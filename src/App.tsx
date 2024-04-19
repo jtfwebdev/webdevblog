3b4623f897e548dc63aeaca00d4160666c4d86f9
Josh Ford <jtfwebdevconsultant@hotmail.com>
2024-04-19 16:51:25 +0100

    added error routing. added new resources to Resources.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { useEffect, useRef, useState, createContext } from 'react';
-import { Outlet, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { Outlet, Routes, Route, RouterProvider, createBrowserRouter, createRoutesFromElements, Link } from 'react-router-dom';
 import 'tailwindcss/tailwind.css';
 import FetchPosts from './assets/Components/FetchPosts';
 import Hero from './assets/Components/Hero';
@@ -35,14 +35,14 @@ function App() {
 
   const postsRef = useRef();
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Root title={title} fetching={fetching} screenWidth={screenWidth} activeBlogTags={activeBlogTags} hamOpen={hamOpen} setHamOpen={setHamOpen} />}>
-        <Route index element={<Content posts={posts} displayedPosts={displayedPosts} setDisplayedPosts={setDisplayedPosts} setActiveBlogTags={setActiveBlogTags} tags={tags} postsRef={postsRef} setTitle={setTitle} />}></Route>
-        <Route path="/blogposts/:blogId" element={<BlogPostFull posts={posts} setTitle={setTitle} setActiveBlogTags={setActiveBlogTags} activeBlogTags={activeBlogTags} tags={tags} />}></Route>
-      </Route>
-    )
-  )
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <Route path="/" element={<Root title={title} fetching={fetching} screenWidth={screenWidth} activeBlogTags={activeBlogTags} hamOpen={hamOpen} setHamOpen={setHamOpen} />}>
+  //       <Route index element={<Content posts={posts} displayedPosts={displayedPosts} setDisplayedPosts={setDisplayedPosts} setActiveBlogTags={setActiveBlogTags} tags={tags} postsRef={postsRef} setTitle={setTitle} />}></Route>
+  //       <Route path="/blogposts/:blogId" element={<BlogPostFull posts={posts} setTitle={setTitle} setActiveBlogTags={setActiveBlogTags} activeBlogTags={activeBlogTags} tags={tags} />}></Route>
+  //     </Route>
+  //   )
+  // )
 
   useEffect(() => { //don't forget to change settings in 'enable CORS' plugin so that only client website can access
     FetchPosts(setPosts, setTags, setDisplayedPosts, setFetching);
@@ -59,25 +59,38 @@ function App() {
   }, [])
 
   return (
-      <RouterProvider router={router} />
+    <>
+      <FetchContext.Provider value={fetching}>
+      <ScreenWidthContext.Provider value={screenWidth}>
+          <AnimatePresence>
+            {hamOpen && <MobileMenuScreen />}
+          </AnimatePresence>
+          {screenWidth > 1040 ? <Header /> : <MobileHeader hamOpen={hamOpen} setHamOpen={setHamOpen} />}
+          <Hero title={title} activeBlogTags={activeBlogTags} />
+          <Routes>
+            <Route index element={<Content posts={posts} displayedPosts={displayedPosts} setDisplayedPosts={setDisplayedPosts} setActiveBlogTags={setActiveBlogTags} tags={tags} postsRef={postsRef} setTitle={setTitle} />}></Route>
+            <Route path="/blogposts/:blogId" element={<BlogPostFull posts={posts} setTitle={setTitle} setActiveBlogTags={setActiveBlogTags} activeBlogTags={activeBlogTags} tags={tags} />}></Route>
+            <Route path="*" element={<RoutingError />}></Route>
+          </Routes>
+      </ScreenWidthContext.Provider>
+      </FetchContext.Provider>
+    </>
   )
 }
 
 export default App;
 
-const Root = ({title, activeBlogTags, fetching, screenWidth, hamOpen, setHamOpen}) => {
-  return (
-    <>
-    <FetchContext.Provider value={fetching}>
-    <ScreenWidthContext.Provider value={screenWidth}>
-        <AnimatePresence>
-          {hamOpen && <MobileMenuScreen />}
-        </AnimatePresence>
-        {screenWidth > 1040 ? <Header /> : <MobileHeader hamOpen={hamOpen} setHamOpen={setHamOpen} />}
-        <Hero title={title} activeBlogTags={activeBlogTags} />
-        <Outlet />
-    </ScreenWidthContext.Provider>
-    </FetchContext.Provider>
-    </>
+const RoutingError = () => {
+
+  const handleClick = () => {
+
+  }
+
+  return(
+    <div className="font-poppins flex flex-col gap-2 w-[80%] mx-auto mt-16 items-start">
+      <h2 className="text-5xl">Oops!</h2>
+      <p>That page could not be found.</p>
+      <Link to={'/'} onClick={handleClick} className="text-[#E88D67]">Return to Homepage</Link>
+    </div>
   )
 }
